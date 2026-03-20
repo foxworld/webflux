09774e0cf14a4dc6203d6f1b77a1ee833ff7387f
이훈구 <foxworld@gmail.com>
2026-03-20 15:24:53 +0900

    reactive study

--- src/test/java/hello/webflux/FunctionalProgrammingTest.java
+++ src/test/java/hello/webflux/FunctionalProgrammingTest.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,25 +25,32 @@ class FunctionalProgrammingTest {
         }
 
         // 모든 값에 *2를 함
-        sink = map(sink, (data) -> data * 4);
-
+        sink = map(sink, (data) -> data * 2);
         // 4의 배수만 남기자
-        sink = filter(sink);
-
-        foreach(sink);
+        sink = filter(sink, (data) -> data % 4 == 0);
+        foreach(sink, (data) -> log.info("value: {}", data));
     }
 
-    private void foreach(List<Integer> sink) {
-        for (Integer integer : sink) {
-            log.info("value:{}", integer);
+    @Test
+    public void produceOneToNineStreamTest() {
+        IntStream.rangeClosed(1,9).boxed()   // loop 1~9
+                .map((data) -> data * 2) // 2 * data 9번 넣기
+                .filter((data) -> data % 4 == 0) // 4의 배수만 찾기
+                .forEach((data) -> log.info("value: {}", data)); // 찾은것을 루프로 출력
+    }
+
+
+    private void foreach(List<Integer> sink, Consumer<Integer> consumer) {
+        for (Integer i : sink) {
+            consumer.accept(i);
         }
     }
 
     @Nonnull
-    private List<Integer> filter(List<Integer> sink) {
+    private List<Integer> filter(List<Integer> sink, Function<Integer, Boolean> predicate) {
         List<Integer> newSink2 = new ArrayList<>();
         for(int i=0; i < 9; i++) {
-            if(sink.get(i) % 4 == 0) {
+            if(predicate.apply(sink.get(i))) {
                 newSink2.add(sink.get(i));
             }
         }
